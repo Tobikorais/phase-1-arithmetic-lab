7e624b0570004405630a78d8d8cb5e0199cb15a1
Andrew Tobiko <andrew.tobiko1@student.moringaschool.com>
2025-03-03 15:00:19 +0300

    Adding files

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Adjust num1 and num2 for multiplication to equal 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// Define random function
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Define num3 and num4 for modulus operation
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+// Define max function
+const max = Math.max(1, 5, 10, 15, 20);
